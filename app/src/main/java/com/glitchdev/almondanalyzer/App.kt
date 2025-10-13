aaa98bed71c2ef0d8c04051100238f7d62d4458c
onewheatnark <leo.bestoneda12@gmail.com>
2025-10-13 22:49:29 +0300

    Initial commit
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle.kts
    #	app/src/main/java/com/glitchdev/almondanalyzer/App.kt
    #	app/src/main/java/com/glitchdev/almondanalyzer/core/data/db/AppDatabase.kt

--- app/src/main/java/com/glitchdev/almondanalyzer/App.kt
+++ app/src/main/java/com/glitchdev/almondanalyzer/App.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.glitchdev.almondanalyzer.core.di.viewModelsModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
