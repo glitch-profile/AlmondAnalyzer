f7f33490fde88205274e059f26227cd7bb38bbcb
Dmitriy Kopeikin <kapew2284@gmail.com>
2025-10-18 21:25:37 +0500

    Revert "Initial commit"
    This reverts commit aaa98bed71c2ef0d8c04051100238f7d62d4458c.

--- app/src/main/java/com/glitchdev/almondanalyzer/App.kt
+++ app/src/main/java/com/glitchdev/almondanalyzer/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.glitchdev.almondanalyzer.core.di.viewModelsModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/glitchdev/almondanalyzer/core/data/db/AppDatabase.kt
+++ app/src/main/java/com/glitchdev/almondanalyzer/core/data/db/AppDatabase.kt
@@ -1,1 +1,14 @@
+package com.glitchdev.almondanalyzer.core.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.glitchdev.almondanalyzer.fields.data.FieldDao
+import com.glitchdev.almondanalyzer.fields.data.FieldEntity
+
+@Database(
+    entities = [FieldEntity::class],
+    version = 1
 )
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun fieldDao(): FieldDao
+}
