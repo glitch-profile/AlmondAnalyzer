7450b00c41acb83c0f4e23f8a30c0c892f7122ec
onewheatnark <leo.bestoneda12@gmail.com>
2025-11-10 20:21:13 +0300

    Add just_a_branch files

--- app/src/main/java/com/glitchdev/almondanalyzer/App.kt
+++ app/src/main/java/com/glitchdev/almondanalyzer/App.kt
@@ -2,8 +2,10 @@ package com.glitchdev.almondanalyzer
 
 import android.app.Application
 import com.glitchdev.almondanalyzer.core.di.viewModelsModule
+import com.glitchdev.almondanalyzer.expenses.di.expenseModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
@@ -11,7 +13,9 @@ class App: Application() {
             androidContext(this@App)
             modules(
                 listOf(
-                    viewModelsModule
+                    coreModule,
+                    fieldModule,
+                    expenseModule,
                 )
             )
         }
